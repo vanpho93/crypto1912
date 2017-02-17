86286402289dd1bc1b9d4ccbbb13c0b51cee1c71
Admin <vanpho02@gmail.com>
2017-02-17 19:59:31 +0700

    enc dec

--- crypto.js
+++ crypto.js
@@ -1,10 +1,20 @@
 let CryptoJS = require('crypto-js');
 const secret = 'HG83249jjbdsafjkK*(*dja-==)';
 
-var ciphertext = CryptoJS.AES.encrypt('my message', 'secret key 123');
+// var ciphertext = CryptoJS.AES.encrypt('my message', 'secret key 123');
+//
+// // console.log(ciphertext.toString());
+//
+// var bytes  = CryptoJS.AES.decrypt(ciphertext.toString(), 'secret key 123');
+// var plaintext = bytes.toString(CryptoJS.enc.Utf8);
+// console.log(plaintext);
 
-// console.log(ciphertext.toString());
+let encrypt = (password) => {
+  let obj = CryptoJS.AES.encrypt(password, secret);
+  return obj.toString();
+}
 
-var bytes  = CryptoJS.AES.decrypt(ciphertext.toString(), 'secret key 123');
-var plaintext = bytes.toString(CryptoJS.enc.Utf8);
-console.log(plaintext);
+let decrypt = (en) => {
+  let bytes  = CryptoJS.AES.decrypt(en, secret);
+  return bytes.toString(CryptoJS.enc.Utf8);
+}
