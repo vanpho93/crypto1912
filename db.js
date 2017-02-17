6389dae5f9cbc37598d065e8461093a3daa6c9b1
Admin <vanpho02@gmail.com>
2017-02-17 20:38:55 +0700

    bind

--- db.js
+++ db.js
@@ -12,14 +12,30 @@ var config = {
 
 var pool = new pg.Pool(config);
 
-function query(sql, cb){
+function query(sql, data, cb){
   pool.connect((err, client, done) => {
-    if(err) return console.log('Loi: ' + err);
+    if(err) return cb(err);
     done();
-    client.query(sql,(err, result) => {
-      if(err) return console.log('Loi ' + err);
+    client.query(sql, data, (err, result) => {
+      if(err) return cb(err)
       //console.log(result.rows);
-      cb(result);
+      cb(undefined, result);
     });
   });
 }
+
+let insertUser = (username, password, phone, cb) => {
+  let sql = `INSERT INTO public."User"(username, "password", phone)
+	VALUES ($1, $2, $3)`;
+  query(sql, [username, password, phone], cb);
+
+}
+
+// let checkUser = (username, password, cb) => {
+//
+// }
+
+insertUser(`j'jj'j`, '123', '09372483', (err, res) => {
+  console.log(err);
+  console.log(res);
+})
