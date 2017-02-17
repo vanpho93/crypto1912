7caebff7b75bd63c42f175424ccb74825220ba8d
Admin <vanpho02@gmail.com>
2017-02-17 20:16:40 +0700

    setup routing

---
+++ index.js
@@ -0,0 +1,20 @@
+let express = require('express');
+let app = express();
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.listen(3000, () => console.log('Server started'));
+
+app.get('/', (req, res) => res.send('Homepage'));
+
+app.get('/dangnhap', (req, res) => res.render('dangnhap'));
+
+app.get('/dangky', (req, res) => res.render('dangky'));
+
+app.post('/xulydangnhap', parser, (req, res) => {
+  res.send('Da dang nhap');
+});
+
+app.post('/xulydangky', parser, (req, res) => {
+  res.send('Dang ky thanh cong');
+});
