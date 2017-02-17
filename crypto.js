7f441067f39e1a298ed32c967bd372e23a08fcc3
Admin <vanpho02@gmail.com>
2017-02-17 19:52:14 +0700

    ma hoa

--- crypto.js
+++ crypto.js
@@ -1,5 +1,10 @@
 let CryptoJS = require('crypto-js');
+const secret = 'HG83249jjbdsafjkK*(*dja-==)';
 
 var ciphertext = CryptoJS.AES.encrypt('my message', 'secret key 123');
 
-console.log(ciphertext);
+// console.log(ciphertext.toString());
+
+var bytes  = CryptoJS.AES.decrypt(ciphertext.toString(), 'secret key 123');
+var plaintext = bytes.toString(CryptoJS.enc.Utf8);
+console.log(plaintext);
