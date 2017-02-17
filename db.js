5d74c1b11236251134ac6251547be4d481ebbfc2
Admin <vanpho02@gmail.com>
2017-02-17 20:56:05 +0700

    export

--- db.js
+++ db.js
@@ -28,14 +28,23 @@ let insertUser = (username, password, phone, cb) => {
   let sql = `INSERT INTO public."User"(username, "password", phone)
 	VALUES ($1, $2, $3)`;
   query(sql, [username, password, phone], cb);
-
 }
 
-// let checkUser = (username, password, cb) => {
-//
-// }
+let checkUser = (username, password, cb) => {
+  let sql = `SELECT * FROM "User" WHERE username = $1 AND "password" = $2`;
+  query(sql, [username, password], (err, result) => {
+    if(err) return cb(err);
+    if(result.rowCount != 1) return cb(new Error('User khong ton tai'));
+    cb(undefined);
+  });
+}
 
-insertUser(`j'jj'j`, '123', '09372483', (err, res) => {
-  console.log(err);
-  console.log(res);
-})
+// insertUser(`j'jj'j`, '123', '09372483', (err, res) => {
+//   console.log(err);
+//   console.log(res);
+// })
+
+// checkUser('Pho', '12223', err => {
+//   if(err) return console.log(err);
+//   console.log('Dang nhap thanh cong');
+// });
