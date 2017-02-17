dffb1edc8ef447ef1f2c0d68954a7dd0248f1a3f
Admin <vanpho02@gmail.com>
2017-02-17 21:10:54 +0700

    xu ly dang ky

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views');
 let parser = require('body-parser').urlencoded({extended: false});
+let {checkUser, insertUser} = require('./db.js');
 
 app.listen(3000, () => console.log('Server started'));
 
@@ -17,5 +18,10 @@ app.post('/xulydangnhap', parser, (req, res) => {
 });
 
 app.post('/xulydangky', parser, (req, res) => {
-  res.send('Dang ky thanh cong');
+  let {username, password, phone} = req.body;
+  insertUser(username, password, phone, (err, result) => {
+    if(err) return res.send(err);
+    if(result.rowCount != 1) return res.send('Loi dang ky');
+    res.send('Dang ky thanh cong');
+  });
 });
